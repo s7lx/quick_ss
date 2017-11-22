6ec3c1b57ce547451d572ae825c260f3b47a531e
s7lx <s7lxhacker@gmail.com>
2017-11-22 15:48:48 +0800

    not copy

--- install_tools.sh
+++ install_tools.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+dirl=`pwd`
+sudo ln -s $dirl/src/runss /usr/bin/runss
+sudo ln -s $dirl/src/runall /usr/bin/runall
+sudo ln -s $dirl/src/log_port /usr/bin/log_port
+sudo ln -s $dirl/src/jsonwrite /usr/bin/jsonwrite
+sudo ln -s $dirl/src/trafic_log /usr/bin/trafic_log
 
-sudo cp src/* /usr/bin/
