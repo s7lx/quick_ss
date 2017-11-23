551a1973cde683ebb602070821ac4e512316a75c
s7lx <s7lxhacker@gmail.com>
2017-11-23 11:25:07 +0800

    create symbolic force

--- install_tools.sh
+++ install_tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dirl=`pwd`
-sudo ln -s $dirl/src/runss /usr/bin/runss
-sudo ln -s $dirl/src/runall /usr/bin/runall
-sudo ln -s $dirl/src/log_port /usr/bin/log_port
-sudo ln -s $dirl/src/jsonwrite /usr/bin/jsonwrite
-sudo ln -s $dirl/src/trafic_log /usr/bin/trafic_log
+sudo ln -f -s $dirl/src/runss /usr/bin/runss
+sudo ln -f -s $dirl/src/runall /usr/bin/runall
+sudo ln -f -s $dirl/src/log_port /usr/bin/log_port
+sudo ln -f -s $dirl/src/jsonwrite /usr/bin/jsonwrite
+sudo ln -f -s $dirl/src/trafic_log /usr/bin/trafic_log
 
