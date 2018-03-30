11fbeca5fcc8da07f9d7a920cc9024b183a7f8f4
s7lx <s7lxhacker@gmail.com>
2018-03-30 16:57:22 +0800

    no message

--- install_tools.sh
+++ install_tools.sh
@@ -5,5 +5,5 @@ sudo ln -f -s $dirl/src/runall /usr/bin/runall
 sudo ln -f -s $dirl/src/log_port /usr/bin/log_port
 sudo ln -f -s $dirl/src/jsonwrite /usr/bin/jsonwrite
 sudo ln -f -s $dirl/src/trafic_log /usr/bin/trafic_log
-sudo ln -f -s $dirl/src/trafic_log /etc/ss_tpl.tpl
+sudo ln -f -s $dirl/src/ss_tpl.tpl /etc/ss_tpl.tpl
 
