bc4dc1d7e1b9eb05acc77d4ef909d81099711016
s7lx <s7lxhacker@gmail.com>
2018-04-11 20:03:24 +0800

    add two tools

--- install_tools.sh
+++ install_tools.sh
@@ -5,6 +5,7 @@ sudo ln -f -s $dirl/src/runall /usr/bin/runall
 sudo ln -f -s $dirl/src/log_port /usr/bin/log_port
 sudo ln -f -s $dirl/src/jsonwrite /usr/bin/jsonwrite
 sudo ln -f -s $dirl/src/trafic_log /usr/bin/trafic_log
+sudo ln -f -s $dirl/src/regex /usr/bin/regex
 sudo ln -f -s $dirl/src/netnow /usr/bin/netnow
 sudo ln -f -s $dirl/src/ss_tpl.tpl /etc/ss_tpl.tpl
 
