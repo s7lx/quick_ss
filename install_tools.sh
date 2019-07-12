f1d44cdd378400073b61ff79bb102a8072c28be2
Richard Zhang <s7lxhacker@gmail.com>
2019-07-12 09:58:39 +0800

    add tools : restore_ss  /  susp_ss ; update install script

--- install_tools.sh
+++ install_tools.sh
@@ -9,4 +9,5 @@ sudo ln -f -s $dirl/src/regex /usr/bin/regex
 sudo ln -f -s $dirl/src/netnow /usr/bin/netnow
 sudo ln -f -s $dirl/src/read_pass /usr/bin/read_pass
 sudo ln -f -s $dirl/src/ss_tpl.tpl /etc/ss_tpl.tpl
-
+sudo ln -f -s $dirl/src/restore_ss /usr/bin/restore_ss
+sudo ln -f -s $dirl/src/susp_ss /usr/bin/susp_ss
