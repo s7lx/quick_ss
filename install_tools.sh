4dbbee1afbc991c2fbabcde9a3d31065b9633236
Richard Zhang <s7lxhacker@gmail.com>
2019-09-23 01:09:10 +0800

    runall_ex install script

--- install_tools.sh
+++ install_tools.sh
@@ -2,6 +2,7 @@
 dirl=`pwd`
 sudo ln -f -s $dirl/src/runss /usr/bin/runss
 sudo ln -f -s $dirl/src/runall /usr/bin/runall
+sudo ln -f -s $dirl/src/runall /usr/bin/runall_ex
 sudo ln -f -s $dirl/src/log_port /usr/bin/log_port
 sudo ln -f -s $dirl/src/jsonwrite /usr/bin/jsonwrite
 sudo ln -f -s $dirl/src/trafic_log /usr/bin/trafic_log
