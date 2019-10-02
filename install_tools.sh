912f0a7087964c577dd52fa6ade68348941cb198
Richard Zhang <s7lxhacker@gmail.com>
2019-10-02 17:12:01 +0800

    install upgrade_ss to link

--- install_tools.sh
+++ install_tools.sh
@@ -12,4 +12,5 @@ sudo ln -f -s $dirl/src/read_pass /usr/bin/read_pass
 sudo ln -f -s $dirl/src/ss_tpl.tpl /etc/ss_tpl.tpl
 sudo ln -f -s $dirl/src/ss_tpl_cpt.tpl /etc/ss_tpl_cpt.tpl
 sudo ln -f -s $dirl/src/restore_ss /usr/bin/restore_ss
+sudo ln -f -s $dirl/src/upgrade_ss /usr/bin/upgrade_ss
 sudo ln -f -s $dirl/src/susp_ss /usr/bin/susp_ss
