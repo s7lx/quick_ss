e0be8af1ad3fd64e21cbf5d4cb1d7dd9891b25da
Richard Zhang <s7lxhacker@gmail.com>
2020-03-25 02:41:53 +0800

    A new GEOIP tool called : asgeo

--- install_tools.sh
+++ install_tools.sh
@@ -7,6 +7,7 @@ sudo ln -f -s $dirl/src/log_port /usr/bin/log_port
 sudo ln -f -s $dirl/src/jsonwrite /usr/bin/jsonwrite
 sudo ln -f -s $dirl/src/trafic_log /usr/bin/trafic_log
 sudo ln -f -s $dirl/src/regex /usr/bin/regex
+sudo ln -f -s $dirl/src/asgeo /usr/bin/asgeo
 sudo ln -f -s $dirl/src/netnow /usr/bin/netnow
 sudo ln -f -s $dirl/src/read_pass /usr/bin/read_pass
 sudo ln -f -s $dirl/src/ss_tpl.tpl /etc/ss_tpl.tpl
