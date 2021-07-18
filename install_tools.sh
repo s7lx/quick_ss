e3709a9b103171a87efe1b456ece8d1d6b10755e
Richard Zhang <s7lxhacker@gmail.com>
2021-07-18 23:14:04 +0800

    add new tools: ss_watch_dog

--- install_tools.sh
+++ install_tools.sh
@@ -10,6 +10,7 @@ sudo ln -f -s $dirl/src/regex /usr/bin/regex
 sudo ln -f -s $dirl/src/asgeo /usr/bin/asgeo
 sudo ln -f -s $dirl/src/netnow /usr/bin/netnow
 sudo ln -f -s $dirl/src/read_pass /usr/bin/read_pass
+sudo ln -f -s $dirl/src/ss_watch_dog /usr/bin/ss_watch_dog
 sudo ln -f -s $dirl/src/ss_tpl.tpl /etc/ss_tpl.tpl
 sudo ln -f -s $dirl/src/ss_tpl_cpt.tpl /etc/ss_tpl_cpt.tpl
 sudo ln -f -s $dirl/src/restore_ss /usr/bin/restore_ss
